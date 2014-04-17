434024441a15794c8afe35cde890b4f56eee0241
Maik Schulze <opensource4maik@t-online.de>
2014-04-17 16:09:16 +0200

    Added android library and an android example project.

---
+++ com.task4java.android/src/com/task4java/android/util/concurrent/HandlerExecutor.java
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (c) 2014 Andree Hagelstein, Maik Schulze, Deutsche Telekom AG. All Rights Reserved.
+ *  
+ *  Filename: HandlerExecutor.java
+ */
+
+package com.task4java.android.util.concurrent;
+
+import java.util.concurrent.Executor;
+
+public class HandlerExecutor implements Executor {
+
+	@Override
+	public void execute(Runnable command) {
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
